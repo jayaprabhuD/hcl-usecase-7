7be5739dc479aa6fa4886c4d3ce28c1f2cf40d11
jayaprabhuD <119481928+jayaprabhuD@users.noreply.github.com>
2025-06-09 16:02:03 +0000

    Adding cloudwatch log group

--- modules/cloudwatch/log_group.tf
+++ modules/cloudwatch/log_group.tf
@@ -1,7 +1,8 @@
 resource "aws_cloudwatch_log_group" "api_gw_logs" {
-  name              = "demo_api_cloudwatch_log_group"
-  retention_in_days = 7
+  name              = "demo_api_cloudwatch_log_group"
+  retention_in_days = 7
 }
+
 
 output "api_log_group_arn" {
     value = aws_cloudwatch_log_group.api_gw_logs.arn
